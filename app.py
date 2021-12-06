b65f30d54caf51865794ca3312b64fc5d9021f3a
zhangdairui <464042992@qq.com>
2021-12-06 22:44:50 +0800

    Study flash - made a addition calculator demo

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 from flask import Flask, flash, redirect, render_template, \
-     request, url_for
+    request, url_for
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+
+# 主页路由
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+
+# 计算器路由
+@app.route('/calculate.html', methods=['GET', 'POST'])
+def addition():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or \
-                request.form['password'] != 'secret':
-            error = 'Invalid credentials'
+
+        if len(request.form['firstNum']) == 0 or \
+                len(request.form['secondNum']) == 0:
+            flash('Input the numbers please!')
         else:
-            flash('You were successfully logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+            result = int(request.form['firstNum']) + int(request.form['secondNum'])
+            # print(result)
+            flash(result)
+            # return redirect(url_for('/calculate.html'))
+    return render_template('/calculate.html', error=error)
+
+# 登录界面路由
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or \
+#                 request.form['password'] != 'secret':
+#             error = 'Invalid credentials'
+#         else:
+#             flash('You were successfully logged in')
+#             return redirect(url_for('index'))
+#     return render_template('calculate.html', error=error)
