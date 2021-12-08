aa135c5c58421185695deef8ba9bfcd6749aaae5
zhangdairui <464042992@qq.com>
2021-12-08 22:04:48 +0800

    Beauty Table Homework

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
-# Flask_study
+# Beauty Table using Bootstrap
 For IS601-851's homework
 
-1. I followed the tutorial and doc. Then made a little demo - a web addition calculator has flash function. Very simple but I learned a lot.
+1. I used Bootstrap to beautiful my tables. The table displayed calculation history.
 
-![fVTcEmGtlAypqgU](https://s2.loli.net/2021/12/06/fVTcEmGtlAypqgU.png)
+Users can click "Check history" to see the history table
 
-2. If you input two numbers and then click "Do Add", you will get the addition result by flash.
+![YciCyLRt3eSGqoF](https://s2.loli.net/2021/12/08/YciCyLRt3eSGqoF.png)
 
-![UkQOhjsu17FXgWi](https://s2.loli.net/2021/12/06/UkQOhjsu17FXgWi.png)
+2. This is the table:
+![DVQ3mat8wrvne79](https://s2.loli.net/2021/12/08/DVQ3mat8wrvne79.png)
 
-3. If you didn't input either of them, you will get a flash to warning to input numbers.
-
-![EScl1BU7wZyex4M](https://s2.loli.net/2021/12/06/EScl1BU7wZyex4M.png)
-
-
-I will try to add flash function to Project 3.
+I will use Bootstrap in Project 3.
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, flash, redirect, render_template, \
     request, url_for
+import time
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+# HISTORY
+
+history_list = []
 
 # 主页路由
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 # 计算器路由
 @app.route('/calculate.html', methods=['GET', 'POST'])
@@ -21,11 +24,32 @@ def addition():
                 len(request.form['secondNum']) == 0:
             flash('Input the numbers please!')
         else:
-            result = int(request.form['firstNum']) + int(request.form['secondNum'])
+            #put the numbers to the list
+
+            cal_history = {'cal_time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}
+            a = request.form['firstNum']
+            cal_history['first_num'] = a
+            b = request.form['secondNum']
+            cal_history['second_num'] = b
+            result = int(a) + int(b)
+            cal_history['operation'] = 'Addition'
+            cal_history['result'] = result
+
             # print(result)
             flash(result)
+            # print(cal_history)
+            history_list.append(cal_history)
+            print(history_list)
             # return redirect(url_for('/calculate.html'))
     return render_template('/calculate.html', error=error)
+
+# history路由
+@app.route('/history', methods=["POST","GET"])
+def history():
+    if request.method == "GET":
+        return render_template("/history.html", his=history_list)
+
+
 
 # 登录界面路由
 # @app.route('/login', methods=['GET', 'POST'])
